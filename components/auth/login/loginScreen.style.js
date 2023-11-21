12d959df46fbb140c0ccf91b97a860a023f38d2e
Ethan Ash <ethanash221@gmail.com>
2023-11-21 11:30:54 -0800

    Optimize login page to use input form components

--- components/auth/login/loginScreen.style.js
+++ components/auth/login/loginScreen.style.js
@@ -16,56 +16,26 @@ const styles = StyleSheet.create({
 		alignSelf: "center",
 		maxWidth: "100%"
 	},
-	formContainer: { 
-		display: "flex",
-		minWidth: "500px",
-		justifyContent: "space-around",
-		'@media (min-width: 500px)': {
-            backgroundColor: 'red',
-        },
-	},
-	button: {
-		alignItems: 'center',
-		justifyContent: 'center',
-		paddingVertical: 12,
-		paddingHorizontal: 32,
-		borderRadius: 4,
-		elevation: 3,
-		backgroundColor: COLORS.secondary,
-	},
-	buttonLabel:{
-		fontSize: 16,
-		lineHeight: 21,
-		fontWeight: 'bold',
-		letterSpacing: 0.25,
-		color: COLORS.lightWhite,
-	},
-	intputLabel: {
-		fontWeight: "bold",
-		color: COLORS.gray,
-		fontSize: SIZES.medium,
-	},
-	input: {
-		height: 40,
-		width: '100%',
-		marginBottom: 5,
-		borderWidth: 1,
-		borderColor: COLORS.gray,
-		padding: SIZES.medium,
-	},
-
-	actionLinks:{
-		display: "flex",
-		paddingTop: SIZES.large,
-		marginBottom: SIZES.small,
-		flex: 1
-	},
-
+	
 	errorMessage:{
-		lineHeight: SIZES.large * 2,
 		color: COLORS.error,
+		fontFamily: FONT.bold,
+		fontSize: FONT.medium,
 		fontWeight: 'bold',
-	}
+		marginTop: "-18px",
+		marginBottom: "20px"
+	},
+
+	actionLinksContainer:{
+		display: "flex",
+		flexDirection: "column"
+	},
+
+	actionLink:{
+		paddingBottom: "12px",
+		fontFamily: FONT.regular,
+		fontSize: FONT.medium,
+	},
 
 })
 
