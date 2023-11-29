8530e7e69cb105404000a4eaf1b65b60feb3ce8f
David Schoenbauer <dschoenbauer@gmail.com>
2023-11-28 21:04:19 -0600

    Creating reset password page with form components (#15)
    Co-authored-by: Ethan Ash <ethanash221@gmail.com>

---
+++ components/form/InputBox.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import { View, Text, Pressable, TextInput} from 'react-native'
+
+import styles from './inputBoxes.style'
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { useTogglePasswordVisibility } from '../../hook/useTogglePasswordVisability';
+
+const InputBox = (props) => {
+    /*
+    prop values:
+
+    labelText
+    passwordVisibility
+    isPassword
+    onChangeText
+    onSubmitEditing
+    inputValue
+    */
+
+    const { passwordVisibility, rightIcon, handlePasswordVisibility } = useTogglePasswordVisibility(props.isPassword);
+
+    return (
+        <View style={styles.inputElement}>
+            <Text style={styles.intputLabel}>{props.labelText || "Input Label"}</Text>
+            <View style={styles.input}>
+                <TextInput
+                    style={styles.inputBox}
+                    secureTextEntry={passwordVisibility}
+                    onChangeText={props.onChangeText}
+                    onSubmitEditing={props.onSubmitEditing}
+                    value={props.inputValue}
+                >
+                </TextInput>
+                { props.isPassword ? 
+                <Pressable style={styles.eyeButton} onPress={handlePasswordVisibility}>
+                    <MaterialCommunityIcons name={rightIcon} size={22} color="grey" />
+                </Pressable>
+                : null }
+            </View>
+        </View>
+    )
+}
+
+export default InputBox
--- enum/lang/en.js
+++ enum/lang/en.js
@@ -1,9 +1,16 @@
 module.exports = {
 	usernameLabel: "Username",
 	passwordLabel: "Password",
+	newPasswordLabel: "New Password",
+	confirmNewPasswordLabel: "Confirm Password",
 	loginLabel: "Login",
 	signUpPrompt: "Don't have an account? Sign-up",
 	forgotPasswordPrompt: "Forgot your password?",
+	passwordResetLabel: "Reset Password",
+	saveLabel: "Save",
+	passwordsDontMatch: "Passwords do not match",
+	resetPasswordFailed: "Unable to reset password",
+	loginFailed: "Username/Password incorrect",
 
 
 	//Error codes
